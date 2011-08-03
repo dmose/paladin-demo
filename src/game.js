be63e962103372381c0485d1afa2950594361391
Bobby Richter <secretrobotron@gmail.com>
2011-08-03 16:09:53 -0400

    Physics changes to protogame.

--- src/game.js
+++ src/game.js
@@ -3,11 +3,14 @@
 function Game() {
 
     var paladin = new Paladin( {debug: true} );
+    var universe = new paladin.physics.Universe();
 
     var scene = new paladin.Scene();
     paladin.graphics.pushScene( scene );
 
     var keysDown = {};
+
+    var shipBody;
     
     var shipEntity = this.entity = new paladin.Entity({
       parent: scene,
@@ -17,7 +20,7 @@ function Game() {
           components: [
             new paladin.component.Camera({
               targeted: false,
-              position: [0, 10, -20],
+              position: [0, 10, -50],
               rotation: [0, 180, 0]
             }), //camera
             new paladin.component.Model( {
@@ -50,15 +53,36 @@ function Game() {
                 entity.spatial.rotation[2] = -cameraRoll;
                 shipModel.object.rotation[2] = -cameraRoll*5;
                 
+                //shipBody.moveAABB( shipModel.object.getAABB() );
               }
             } );
 
+            shipBody = new paladin.physics.Body({
+              aabb: shipModel.object.getAABB()
+            });
+
+            universe.addBody( shipBody );
+
           }
         }),
-      ],
+      ], //children
+      listeners: {
+        'a-up': function ( params ) {
+          keysDown['a'] = false;
+        },
+        'a-down': function ( params ) {
+          keysDown['a'] = true;
+        },
+        'd-up': function ( params ) {
+          keysDown['d'] = false;
+        },
+        'd-down': function ( params ) {
+          keysDown['d'] = true;
+        }
+      },
       
       init: function ( entity ) {
-        var accel = 0.01;
+        var accel = 5;
         var shipFlyingTask = paladin.tasker.add( {
             callback: function ( task ) {
                 var rotY = entity.spatial.rotation[1];
@@ -70,56 +94,18 @@ function Game() {
                 ];
 
                 dirVec = CubicVR.vec3.normalize(dirVec);
+                dirVec = CubicVR.vec3.multiply(dirVec, accel);
 
-                entity.spatial.position[0] += dirVec[0] * accel * task.dt;
-                entity.spatial.position[2] += dirVec[2] * accel * task.dt;
+                //entity.spatial.position[0] += dirVec[0] * task.dt;
+                //entity.spatial.position[2] += dirVec[2] * task.dt;
+                
+                shipBody.setVelocity(dirVec);
 
-            }
-        } );
-        entity.listen( {
-            event: paladin.keyboardInput.Event( ['a'], false ), 
-            callback: function( p ) {
-                keysDown['a'] = false;
-            }
-        } );
-        entity.listen( {
-            event: paladin.keyboardInput.Event( ['a'], true ), 
-            callback: function( p ) {
-                keysDown['a'] = true;
-            }
-        } );
-        entity.listen( {
-            event: paladin.keyboardInput.Event( ['d'], false ), 
-            callback: function( p ) {
-                keysDown['d'] = false;
-            }
-        } );
-        entity.listen( {
-            event: paladin.keyboardInput.Event( ['d'], true ), 
-            callback: function( p ) {
-                keysDown['d'] = true;
-            }
-        } );
-        entity.listen( {
-            event: paladin.touchInput.Event( [], true ),
-            callback: function( p ) { 
-                var position = p[0].position;
-                var width = paladin.graphics.getWidth();
-                if( position.x < width/2 )
-                    keysDown['a'] = true;
-                else if( position.x > width/2 )
-                    keysDown['d'] = true;
-            }
-        } );
-        entity.listen( {
-            event: paladin.touchInput.Event( [], false ),
-            callback: function( p ) { 
-                var position = p[0].position;
-                var width = paladin.graphics.getWidth();
-                if( position.x < width/2 )
-                    keysDown['a'] = false;
-                else if( position.x > width/2 )
-                    keysDown['d'] = false;
+                var dims = shipBody.getSphere().getDims();
+                entity.spatial.position[0] = dims[0];
+                entity.spatial.position[1] = dims[1];
+                entity.spatial.position[2] = dims[2];
+
             }
         } );
       }
@@ -127,12 +113,12 @@ function Game() {
     
 
     var boxes = [];
-    for (var i=0; i<100; ++i) {
+    for (var i=0; i<10; ++i) {
       (function () {
         var mesh = new paladin.graphics.Mesh( {
             primitives: [ {
                 type: 'box',
-                size: 0.5 + Math.random(),
+                size: 5 + Math.random(),
                 material: {
                   color: [Math.random(), Math.random(), Math.random()]
                 }
@@ -141,18 +127,30 @@ function Game() {
         } );
 
         var box = new paladin.Entity();
-        box.addComponent( new paladin.component.Model( {
+        var model = new paladin.component.Model( {
             mesh: mesh
-        } ) );
-        box.spatial.position = [-50 + 100 * Math.random(), 
+        } );
+        box.addComponent( model );
+        box.spatial.position = [-100 + 200 * Math.random(), 
                                 -5 + 10 * Math.random(),
-                                -50 + 100 * Math.random()];
+                                -100 + 200 * Math.random()];
+
         box.spatial.rotation = [Math.random() * 360,
                                 Math.random() * 360,
                                 Math.random() * 360];
         box.setParent( scene );
+
+        scene.graphics.prepareTransforms();
         
         boxes.push(box);
+
+        box.body = new paladin.physics.Body({
+          aabb: model.object.getAABB()
+        });
+
+        box.body.externalObject = box;
+
+        universe.addBody( box.body );
       
       })();
     } //for
@@ -166,12 +164,123 @@ function Game() {
           }
         }
     } );
+
+    // This shouldn't be CubicVR code...
+    var explosionMesh = CubicVR.primitives.box({
+      size: 5+Math.random(),
+      transform: (new CubicVR.Transform()).rotate([
+                    Math.random()*360, 
+                    Math.random()*360, 
+                    Math.random()*360]).translate([
+                    -.5+Math.random()*.5, 
+                    -.5+Math.random()*.5, 
+                    -.5+Math.random()*.5]),
+      material: new CubicVR.Material({
+        specular: [1, 1, 1],
+        shininess: 1.0,
+        env_amount: 0.5,
+        color: [Math.random()*.2+.8, Math.random()*.1, 0],
+        opacity: 0.1,
+        textures: {
+          envsphere: new CubicVR.Texture('fract_reflections.jpg'),
+          alpha: new CubicVR.Texture('fract_reflections.jpg')
+        }
+      }),
+      uvmapper: {
+        projectionMode: CubicVR.enums.uv.projection.CUBIC,
+        scale:[5, 5, 5]
+      }
+    });
+    CubicVR.primitives.box({
+      mesh: explosionMesh,
+      size: 5+Math.random(),
+      transform: (new CubicVR.Transform()).rotate([
+                    Math.random()*360, 
+                    Math.random()*360, 
+                    Math.random()*360]).translate([
+                    -.5+Math.random()*.5, 
+                    -.5+Math.random()*.5, 
+                    -.5+Math.random()*.5]),
+      material: new CubicVR.Material({
+        env_amount: 1.0,
+        color: [Math.random()*.2+.8, Math.random()*.2+.6, 0],
+        env_amount: .5,
+        opacity: 0.1,
+        textures: {
+          envsphere: new CubicVR.Texture('fract_reflections.jpg'),
+          alpha: new CubicVR.Texture('fract_reflections.jpg')
+        }
+      }),
+      uvmapper: {
+        projectionMode: CubicVR.enums.uv.projection.CUBIC,
+        scale:[5, 5, 5]
+      }
+    });
+    CubicVR.primitives.sphere({
+      mesh: explosionMesh,
+      lon: 24,
+      lat: 24,
+      radius: 6+Math.random(),
+      transform: (new CubicVR.Transform()).rotate([Math.random()*360, Math.random()*360, Math.random()*360]),
+      material: new CubicVR.Material({
+        opacity: .999,
+        color: [Math.random()*.2+.8, Math.random()*.2+.3, 0],
+        textures: {
+          alpha: new CubicVR.Texture('fract_reflections.jpg')
+        }
+      }),
+      uvmapper: {
+        projectionMode: CubicVR.enums.uv.projection.CUBIC,
+        scale:[5, 5, 5]
+      }
+    });
+    explosionMesh.prepare();
+
     
     this.run = function () {
+      paladin.tasker.add({
+        callback: function ( task ) {
+          var collisions = universe.advance( task.dt/100 );
+          if ( shipBody.collisions.length > 0 ) {
+            for ( var i=0, l=shipBody.collisions.length; i<l; ++i ) {
+              var entity = shipBody.collisions[i].externalObject;
+              entity.spatial.position[1] = 200;
+              universe.removeBody( shipBody.collisions[i] );
+              paladin.tasker.add((function () {
+                var explosionObject = new CubicVR.SceneObject( explosionMesh );
+                explosionObject.position = shipEntity.spatial.position.slice();
+                scene.graphics.bindSceneObject(explosionObject);
+                explosionObject.rotation = [
+                  Math.random()*360,
+                  Math.random()*360,
+                  Math.random()*360
+                ];
+                explosionObject.scale = [.1, .1, .1];
+                return { callback: function ( task ) {
+                  var s = 40 - 40/(Math.pow(Math.E, task.elapsed/1000));
+                  explosionObject.scale = [s, s, s];
+                  if ( task.elapsed > 2000 ) {
+                    scene.graphics.removeSceneObject( explosionObject );
+                    return task.DONE;
+                  }
+                  return task.CONT;
+                }}
+              })());
+            }
+          }
+        }
+      });
       paladin.run();
     };
 
-    window.foo = scene.graphics;
+    scene.graphics.bindLight( new CubicVR.Light({
+      type: CubicVR.enums.light.type.AREA,
+      intensity:0.9,
+      mapRes:1024,
+      areaCeiling:40,
+      areaFloor:-40,
+      areaAxis:[25,5]
+    }));
 
 };
 
